ef8adc8eb08a784dbeb69a2afe4d7ba1f60bcff9
Hyebin (Helia) <hyebin218@naver.com>
2024-01-11 15:31:10 +0900

    init: set SwiftLint

---
+++ ForestTori/ForestTori/ForestToriApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestToriApp.swift
+//  ForestTori
+//
+//  Created by hyebin on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ForestToriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
